bedf7b181a9945a6ccdeb5e66cad7423e3750513
James Holloway <james.holloway94@gmail.com>
2026-04-28 14:40:18 +0000

    preserve mysql user permission sync in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-28 13:49:25 UTC
+# Last sync: 2026-04-28 14:40:18 UTC
