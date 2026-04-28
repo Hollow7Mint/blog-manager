35eea38f396c28ecccc420f9fb54fdd0378dc4bf
James Holloway <james.holloway94@gmail.com>
2026-04-28 18:16:31 +0000

    flush ec2 fleet inventory marker after scan

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-28 18:00:05 UTC
+# Last sync: 2026-04-28 18:16:23 UTC
