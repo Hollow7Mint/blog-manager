e55951f9dd6f317fd0283f811fe756474f85eaac
James Holloway <james.holloway94@gmail.com>
2026-04-28 20:40:04 +0000

    harvest ssh fingerprint sync for nightly audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-28 19:27:37 UTC
+# Last sync: 2026-04-28 20:40:04 UTC
