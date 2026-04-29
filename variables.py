fd7f1b7861d923eb05b4e15e3a4886de3d37e751
James Holloway <james.holloway94@gmail.com>
2026-04-29 13:24:22 +0000

    flag cloudwatch log retention sync in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-29 12:09:50 UTC
+# Last sync: 2026-04-29 13:24:22 UTC
