ddce67130e6661b5c3b9315b94d5f1f93f35c432
James Holloway <james.holloway94@gmail.com>
2026-04-29 22:09:14 +0000

    queue ssh agent forwarding sync after config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-29 21:33:43 UTC
+# Last sync: 2026-04-29 22:09:14 UTC
