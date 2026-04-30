4ebc680a1b47ba2905c098299f673dc04de4ef52
James Holloway <james.holloway94@gmail.com>
2026-04-30 02:48:41 +0000

    rotate kms key metadata sync after policy apply

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-30 01:13:38 UTC
+# Last sync: 2026-04-30 02:48:41 UTC
