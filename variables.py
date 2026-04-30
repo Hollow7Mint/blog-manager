95b4ea4e5c21a7928d7163d295475d800ae28c20
James Holloway <james.holloway94@gmail.com>
2026-04-30 12:51:03 +0000

    frame ssh agent socket sync in config record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-04-30 12:03:36 UTC
+# Last sync: 2026-04-30 12:51:03 UTC
