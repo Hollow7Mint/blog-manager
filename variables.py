51cbdc6e2b7eefc7e96e7090e31e9d435338a4e5
James Holloway <james.holloway94@gmail.com>
2026-05-01 17:59:55 +0000

    relay db connection string time through config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-01 16:48:22 UTC
+# Last sync: 2026-05-01 17:59:55 UTC
