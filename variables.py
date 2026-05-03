2da59d1de371653f87e82cf647696c286c29d0df
James Holloway <james.holloway94@gmail.com>
2026-05-03 05:42:54 +0000

    thread cloudtrail digest sync through config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-03 04:57:05 UTC
+# Last sync: 2026-05-03 05:42:54 UTC
