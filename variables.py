46aec35214955b4fe89dc8ab974bc0d695b8e4a3
James Holloway <james.holloway94@gmail.com>
2026-05-04 18:41:46 +0000

    carry bastion config sync stamp to metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-04 17:16:06 UTC
+# Last sync: 2026-05-04 18:41:45 UTC
