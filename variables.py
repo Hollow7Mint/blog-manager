c626ac37a74600aaa9e72292e9f4f95a77ddbc29
James Holloway <james.holloway94@gmail.com>
2026-05-04 00:10:54 +0000

    pin bastion ip allowlist time in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-03 23:16:44 UTC
+# Last sync: 2026-05-04 00:10:54 UTC
