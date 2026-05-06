f4eef5e6ef9807339c8ed3d2685a023f9ecd3d0d
James Holloway <james.holloway94@gmail.com>
2026-05-06 05:13:31 +0000

    document postgres index maintenance sync in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-06 04:30:10 UTC
+# Last sync: 2026-05-06 05:13:31 UTC
