791d7b06d715f7e608980788eea48ca96fea55e1
James Holloway <james.holloway94@gmail.com>
2026-05-06 08:51:55 +0000

    resolve waf rule group sync into metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-06 08:06:43 UTC
+# Last sync: 2026-05-06 08:51:55 UTC
