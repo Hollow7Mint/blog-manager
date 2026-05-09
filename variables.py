59de8103e40dbf99182950b322507b8212a4771c
James Holloway <james.holloway94@gmail.com>
2026-05-09 13:00:05 +0000

    prime bastion connection sync marker after refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-09 11:39:49 UTC
+# Last sync: 2026-05-09 13:00:05 UTC
