51bca46292f7188111e23ae10ce4ce040ffafd26
James Holloway <james.holloway94@gmail.com>
2026-05-11 03:18:22 +0000

    update iam policy checkpoint for scheduled audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-11 02:42:21 UTC
+# Last sync: 2026-05-11 03:18:22 UTC
