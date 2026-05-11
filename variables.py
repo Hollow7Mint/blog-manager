3ce1112e155965ab227bf1b712681cb6c41f8d46
James Holloway <james.holloway94@gmail.com>
2026-05-11 15:34:22 +0000

    resolve known_hosts cleanup sync into metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-11 14:29:30 UTC
+# Last sync: 2026-05-11 15:34:22 UTC
