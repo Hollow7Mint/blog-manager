e76d31803eddc55ad6d9cf73a5d936a16fe0903d
James Holloway <james.holloway94@gmail.com>
2026-05-11 18:59:02 +0000

    advance mysql gtid sync into config record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-11 17:23:50 UTC
+# Last sync: 2026-05-11 18:59:02 UTC
