177b2131b9f7532f99314e8edfa5f3d46f775bc4
James Holloway <james.holloway94@gmail.com>
2026-05-12 04:53:47 +0000

    resolve mysql backup sync into metadata file

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-12 03:19:21 UTC
+# Last sync: 2026-05-12 04:53:47 UTC
