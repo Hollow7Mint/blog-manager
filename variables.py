dee5ca0be765edad416db03e4ac10541378f0a88
James Holloway <james.holloway94@gmail.com>
2026-05-13 21:18:31 +0000

    save inspector assessment marker after scan window

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-13 20:29:03 UTC
+# Last sync: 2026-05-13 21:18:31 UTC
