575683255cd5acebf7722cda666f3ab92897e591
James Holloway <james.holloway94@gmail.com>
2026-05-13 11:05:05 +0000

    unify ec2 placement sync with metadata heartbeat

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-13 09:08:22 UTC
+# Last sync: 2026-05-13 11:05:05 UTC
