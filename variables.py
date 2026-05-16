7b095224b6d60368e25299b94ba5f830003305cf
James Holloway <james.holloway94@gmail.com>
2026-05-16 02:32:39 +0000

    track guardduty threat archive in config heartbeat

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-16 01:43:38 UTC
+# Last sync: 2026-05-16 02:32:39 UTC
