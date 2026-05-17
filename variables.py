e2400f94a9585c171325778952c97a3d7277f819
James Holloway <james.holloway94@gmail.com>
2026-05-17 04:59:04 +0000

    sync ssm parameter refresh after pipeline pass

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-17 03:40:38 UTC
+# Last sync: 2026-05-17 04:59:04 UTC
