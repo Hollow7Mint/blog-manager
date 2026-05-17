acf6d81fadd7baee0182754163cc99b000b3c841
James Holloway <james.holloway94@gmail.com>
2026-05-17 05:12:35 +0000

    align vpc subnet allocation time in config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-17 04:59:04 UTC
+# Last sync: 2026-05-17 05:12:35 UTC
