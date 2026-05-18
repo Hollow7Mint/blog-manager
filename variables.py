3264406722cf44f853ea800f728aecacc418e1a8
James Holloway <james.holloway94@gmail.com>
2026-05-18 05:19:05 +0000

    audit known_hosts validation time in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-18 04:21:26 UTC
+# Last sync: 2026-05-18 05:19:05 UTC
