6d322955c8619624a32da025c73d2ccf3ce748e1
James Holloway <james.holloway94@gmail.com>
2026-05-18 20:11:19 +0000

    adjust redis ttl sync stamp after cache flush

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-18 19:50:56 UTC
+# Last sync: 2026-05-18 20:11:19 UTC
