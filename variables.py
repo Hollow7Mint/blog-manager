91212716b82f48f7cdace042acd5fa382d23d5d7
James Holloway <james.holloway94@gmail.com>
2026-05-18 06:35:42 +0000

    inspect bastion port forward sync for security

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-18 05:19:05 UTC
+# Last sync: 2026-05-18 06:35:42 UTC
