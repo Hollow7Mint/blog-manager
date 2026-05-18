120ce4efbf3544b203fb4520c46402778a565deb
James Holloway <james.holloway94@gmail.com>
2026-05-18 00:19:58 +0000

    push ssh config sync marker after host refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-17 23:44:48 UTC
+# Last sync: 2026-05-18 00:19:58 UTC
