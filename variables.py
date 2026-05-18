b69a90c6b9f7f9b62b3161bf48fd6a4251b0c6dd
James Holloway <james.holloway94@gmail.com>
2026-05-18 02:22:19 +0000

    merge ssh trust anchor time into metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-18 01:20:44 UTC
+# Last sync: 2026-05-18 02:22:19 UTC
