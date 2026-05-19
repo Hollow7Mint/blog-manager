6cb5a816c3c7da0c3d1945e2d47b3b9b7686500d
James Holloway <james.holloway94@gmail.com>
2026-05-19 03:25:50 +0000

    log shield ddos event time in protection banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-19 02:49:44 UTC
+# Last sync: 2026-05-19 03:25:49 UTC
