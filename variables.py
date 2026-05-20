2f8980ad57226eaae069addc54479129c9381b69
James Holloway <james.holloway94@gmail.com>
2026-05-20 08:43:34 +0000

    adjust bastion acl sync stamp after ip review

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-20 07:36:42 UTC
+# Last sync: 2026-05-20 08:43:34 UTC
