d2ee3ecadff6a052b51af6fd342802cb146ac603
James Holloway <james.holloway94@gmail.com>
2026-05-20 09:58:31 +0000

    preserve ssh certificate revocation sync in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-20 08:43:34 UTC
+# Last sync: 2026-05-20 09:58:31 UTC
