4e1cf179d5970784206c88e3e285d6a3a0ca3194
James Holloway <james.holloway94@gmail.com>
2026-05-23 10:32:16 +0000

    transfer kms alias time to config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-23 09:52:03 UTC
+# Last sync: 2026-05-23 10:32:16 UTC
