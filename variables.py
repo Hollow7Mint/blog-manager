cd45eeee1c9d3a5cfd40827bb862786fb13a0b51
James Holloway <james.holloway94@gmail.com>
2026-05-24 17:41:10 +0000

    relay ssh host key time through config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-24 16:42:25 UTC
+# Last sync: 2026-05-24 17:41:10 UTC
