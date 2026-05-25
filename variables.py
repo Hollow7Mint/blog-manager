0e65e041ecfeac49ca2246d91de984ebee725b44
James Holloway <james.holloway94@gmail.com>
2026-05-25 08:46:29 +0000

    write mongodb index rebuild time into metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-25 07:50:27 UTC
+# Last sync: 2026-05-25 08:46:29 UTC
