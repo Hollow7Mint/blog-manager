0bf5058b4eb6e5123b2e3fd825cf72f67fa5bfa5
James Holloway <james.holloway94@gmail.com>
2026-05-26 22:14:18 +0000

    set redis cluster sync stamp in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-26 21:42:08 UTC
+# Last sync: 2026-05-26 22:14:18 UTC
