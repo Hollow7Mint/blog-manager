a6de4b7f0d27ae0093452af5b1ceaa254b953203
James Holloway <james.holloway94@gmail.com>
2026-05-26 04:55:21 +0000

    post postgres replication heartbeat to config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-26 03:42:23 UTC
+# Last sync: 2026-05-26 04:55:21 UTC
