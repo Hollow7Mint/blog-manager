d6ce8680803721528fdbf961a33178f7246ec3e3
James Holloway <james.holloway94@gmail.com>
2026-05-27 17:24:49 +0000

    finish secrets manager rotation heartbeat after sync

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-27 17:04:06 UTC
+# Last sync: 2026-05-27 17:24:49 UTC
