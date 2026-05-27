0c93197149c2e57ae0d8c8f5799d3605f4ebde06
James Holloway <james.holloway94@gmail.com>
2026-05-27 21:03:09 +0000

    seal bastion session sync in security banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-27 19:41:02 UTC
+# Last sync: 2026-05-27 21:03:09 UTC
