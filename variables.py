c846e49f46d63ebe561530619c4f64f70ac08f0a
James Holloway <james.holloway94@gmail.com>
2026-05-28 17:57:55 +0000

    publish lambda alias promotion marker after release

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-28 17:03:39 UTC
+# Last sync: 2026-05-28 17:57:55 UTC
