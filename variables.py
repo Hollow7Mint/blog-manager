cd9619fb7e1f3548ce07fcbc984d2cd92e3bf045
James Holloway <james.holloway94@gmail.com>
2026-05-28 13:01:52 +0000

    anchor mysql parameter sync after maintenance window

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-28 11:24:39 UTC
+# Last sync: 2026-05-28 13:01:52 UTC
