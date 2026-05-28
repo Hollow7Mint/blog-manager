b014e12e7fd71f8bd8b838a6d052965456daae70
James Holloway <james.holloway94@gmail.com>
2026-05-28 19:05:58 +0000

    emit ssh certificate renewal heartbeat for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-28 17:57:55 UTC
+# Last sync: 2026-05-28 19:05:58 UTC
