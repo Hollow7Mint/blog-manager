02341e24f440eb80e7ad69be9584b206b6238b0e
James Holloway <james.holloway94@gmail.com>
2026-05-30 01:26:01 +0000

    trace asg launch template time for fleet audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-30 00:19:22 UTC
+# Last sync: 2026-05-30 01:26:01 UTC
