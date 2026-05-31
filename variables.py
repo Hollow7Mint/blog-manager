7891173674765c6029cf9e48020c662c0e1c5c5d
James Holloway <james.holloway94@gmail.com>
2026-05-31 21:14:27 +0000

    persist vpc route table sync after update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-31 21:07:02 UTC
+# Last sync: 2026-05-31 21:14:27 UTC
