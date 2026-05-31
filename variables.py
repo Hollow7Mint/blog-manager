cc2f6e467f7a56bcf712fc5e35e0ad3a55067131
James Holloway <james.holloway94@gmail.com>
2026-05-31 00:04:38 +0000

    seal inspector finding sync in security banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-30 23:03:09 UTC
+# Last sync: 2026-05-31 00:04:38 UTC
