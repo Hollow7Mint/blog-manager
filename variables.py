ac8840a2141a943343c632902e21c500445dace6
James Holloway <james.holloway94@gmail.com>
2026-05-31 13:19:24 +0000

    persist tunnel config refresh in ssh banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-31 13:06:36 UTC
+# Last sync: 2026-05-31 13:19:24 UTC
