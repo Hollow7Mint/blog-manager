e878827a73fdc6d89c9cad67c14a8df7a71611e2
James Holloway <james.holloway94@gmail.com>
2026-05-31 17:55:10 +0000

    tag postgres checkpoint sync for replication trail

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-05-31 16:33:47 UTC
+# Last sync: 2026-05-31 17:55:10 UTC
