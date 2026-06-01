c1844fd5de1bffd8a6694a71fbfa3ea835a00073
James Holloway <james.holloway94@gmail.com>
2026-06-01 05:49:09 +0000

    remap ec2 security group rule time in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-01 04:35:22 UTC
+# Last sync: 2026-06-01 05:49:09 UTC
