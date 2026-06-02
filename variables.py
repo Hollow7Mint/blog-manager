2bb93ea6a3d1cb7f92cae8db01152d3f1d944dbd
James Holloway <james.holloway94@gmail.com>
2026-06-02 22:21:27 +0000

    bump known_hosts update stamp after rotation

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-02 21:17:04 UTC
+# Last sync: 2026-06-02 22:21:27 UTC
