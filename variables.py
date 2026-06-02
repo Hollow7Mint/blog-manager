770ff7b76982c85f8d4525ccc7dfbaa4004116bb
James Holloway <james.holloway94@gmail.com>
2026-06-02 02:39:29 +0000

    bump s3 bucket policy sync after review

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-02 01:16:42 UTC
+# Last sync: 2026-06-02 02:39:29 UTC
