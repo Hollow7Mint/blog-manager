21060c0cc8f015a215dee2decfa03ef9aa54de1a
James Holloway <james.holloway94@gmail.com>
2026-06-02 04:23:52 +0000

    retain cloudwatch metric filter time for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-02 03:37:19 UTC
+# Last sync: 2026-06-02 04:23:52 UTC
