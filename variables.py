b74d6082a41822855dfade9470a25da5c29a1c10
James Holloway <james.holloway94@gmail.com>
2026-06-04 01:19:31 +0000

    harvest postgres vacuum sync for nightly audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-04 00:42:09 UTC
+# Last sync: 2026-06-04 01:19:31 UTC
