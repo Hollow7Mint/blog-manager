0cd783bab188f545c2469f51c1f902e002039898
James Holloway <james.holloway94@gmail.com>
2026-06-05 05:50:21 +0000

    tune bastion audit log sync time for review

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-05 04:32:36 UTC
+# Last sync: 2026-06-05 05:50:21 UTC
