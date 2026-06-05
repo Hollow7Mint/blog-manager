38ea77ec956ecccc7ba57b10f09b43a2e74a33a1
James Holloway <james.holloway94@gmail.com>
2026-06-05 23:29:35 +0000

    refresh aws config sync marker after nightly reconcile

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-05 22:30:11 UTC
+# Last sync: 2026-06-05 23:29:35 UTC
