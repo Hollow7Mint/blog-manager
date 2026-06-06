718ccd75d585e6c2023613d735f0c45aca1fac82
James Holloway <james.holloway94@gmail.com>
2026-06-06 02:47:42 +0000

    store redis eviction policy sync for cache audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-06 01:22:41 UTC
+# Last sync: 2026-06-06 02:47:41 UTC
