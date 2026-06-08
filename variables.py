831af4600cb38fbaf812e5d8d0c99a73558b5968
James Holloway <james.holloway94@gmail.com>
2026-06-08 12:06:13 +0000

    capture host key sync completion time in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-08 10:47:49 UTC
+# Last sync: 2026-06-08 12:06:12 UTC
