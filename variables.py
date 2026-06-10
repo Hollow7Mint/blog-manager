1dd2bb1e96f196ad540c8c72ef7f173df95e819d
James Holloway <james.holloway94@gmail.com>
2026-06-10 01:44:55 +0000

    carry mongodb oplog sync stamp to config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-10 00:36:38 UTC
+# Last sync: 2026-06-10 01:44:55 UTC
