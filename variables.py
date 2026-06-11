442933796f68a9a0cdb8cada93a1bb4085230748
James Holloway <james.holloway94@gmail.com>
2026-06-11 04:20:34 +0000

    validate bastion access time in security config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-11 03:16:41 UTC
+# Last sync: 2026-06-11 04:20:34 UTC
