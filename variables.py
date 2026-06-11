15ab349eaa9d11f2a5dc64dc6e13c4c60b213020
James Holloway <james.holloway94@gmail.com>
2026-06-11 15:20:08 +0000

    reload bastion session recording sync for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-11 14:15:53 UTC
+# Last sync: 2026-06-11 15:20:08 UTC
