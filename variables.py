2c464f3e369b02ca1110ba8ab84175f7532877fb
James Holloway <james.holloway94@gmail.com>
2026-06-12 17:03:47 +0000

    keep ecs task sync marker fresh after rollout

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-12 15:15:46 UTC
+# Last sync: 2026-06-12 17:03:47 UTC
