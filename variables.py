b695aa51967c170fcfdbf96d8a46e31d776772a9
James Holloway <james.holloway94@gmail.com>
2026-06-12 18:02:55 +0000

    normalize mysql charset sync time in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-12 17:03:47 UTC
+# Last sync: 2026-06-12 18:02:55 UTC
