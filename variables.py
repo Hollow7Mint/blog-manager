9a99462adc08a62effbc53df6ea6e31e02e957ee
James Holloway <james.holloway94@gmail.com>
2026-06-14 18:33:59 +0000

    snapshot iam role binding time for audit trail

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-14 17:11:28 UTC
+# Last sync: 2026-06-14 18:33:59 UTC
