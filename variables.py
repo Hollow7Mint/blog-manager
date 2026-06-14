14553763adc3d162b86c517dcbb292e1d3c7e4ac
James Holloway <james.holloway94@gmail.com>
2026-06-14 01:24:39 +0000

    document ssh host key sync time in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-14 00:40:28 UTC
+# Last sync: 2026-06-14 01:24:39 UTC
