d87e7913b6b7f7568325bb58fbb0ca862f09c790
James Holloway <james.holloway94@gmail.com>
2026-06-15 00:39:51 +0000

    unify ssh sync record with metadata heartbeat

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-15 00:07:04 UTC
+# Last sync: 2026-06-15 00:39:51 UTC
