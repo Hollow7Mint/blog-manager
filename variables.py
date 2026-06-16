66d873fa2e18432ef3d6fe7e1f092a0e89a439a7
James Holloway <james.holloway94@gmail.com>
2026-06-16 08:53:07 +0000

    annotate mysql slow log sync time for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-16 07:20:42 UTC
+# Last sync: 2026-06-16 08:53:07 UTC
