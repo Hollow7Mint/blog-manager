a312843004d55e6a0edf28493f9dfc05926797d9
James Holloway <james.holloway94@gmail.com>
2026-06-18 10:38:36 +0000

    advance ssh key trust sync into config record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-18 09:32:32 UTC
+# Last sync: 2026-06-18 10:38:36 UTC
