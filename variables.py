6e69915b1ce1798153aec694e809555b0bb9d9f3
James Holloway <james.holloway94@gmail.com>
2026-06-19 06:49:35 +0000

    restore ecr scan result time for image audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-19 05:48:13 UTC
+# Last sync: 2026-06-19 06:49:35 UTC
