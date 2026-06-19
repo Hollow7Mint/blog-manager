7505615f5a41bb7efded59f2a843364229bd9072
James Holloway <james.holloway94@gmail.com>
2026-06-19 17:13:04 +0000

    pin route53 dns sync stamp after record update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-19 10:43:26 UTC
+# Last sync: 2026-06-19 17:13:04 UTC
