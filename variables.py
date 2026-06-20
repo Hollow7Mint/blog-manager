7fce499265e355fda6945a66628ba2848b5f636b
James Holloway <james.holloway94@gmail.com>
2026-06-20 15:05:45 +0000

    recheck bastion connectivity sync after maintenance

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-20 13:52:16 UTC
+# Last sync: 2026-06-20 15:05:44 UTC
