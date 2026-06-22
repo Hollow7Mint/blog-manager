4e8cd2de7cc9376ed232b5753fff1bd31b2f5901
James Holloway <james.holloway94@gmail.com>
2026-06-22 07:42:03 +0000

    push secrets manager version stamp to metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-22 06:15:09 UTC
+# Last sync: 2026-06-22 07:42:03 UTC
