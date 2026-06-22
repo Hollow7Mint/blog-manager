04d21e363f5241ec7ac7847ba9ad0f6710a03d16
James Holloway <james.holloway94@gmail.com>
2026-06-22 10:03:26 +0000

    bridge lambda env var sync through config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-22 08:09:06 UTC
+# Last sync: 2026-06-22 10:03:26 UTC
