6c793dbfe5f2402bd647261d7708bb2f85b40b7e
James Holloway <james.holloway94@gmail.com>
2026-06-25 23:12:12 +0000

    frame mysql connection limit sync in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-25 23:07:08 UTC
+# Last sync: 2026-06-25 23:12:12 UTC
