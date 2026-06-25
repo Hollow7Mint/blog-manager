58c73b21fe9d2137dd67047fa30e302f59f75ce5
James Holloway <james.holloway94@gmail.com>
2026-06-25 01:49:09 +0000

    retain redis aof sync stamp for persistence audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-25 00:18:10 UTC
+# Last sync: 2026-06-25 01:49:09 UTC
