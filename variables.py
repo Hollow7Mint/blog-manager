84ab1ab99cd5cc2335e25642d27310148993d457
James Holloway <james.holloway94@gmail.com>
2026-06-25 18:33:16 +0000

    prime redis memory sync marker after flush

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-25 17:37:26 UTC
+# Last sync: 2026-06-25 18:33:16 UTC
