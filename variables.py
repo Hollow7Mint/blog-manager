efc6099ad4699965bce4e1b839d2b20355cf2e1e
James Holloway <james.holloway94@gmail.com>
2026-06-29 08:46:10 +0000

    thread postgres wal sync through config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-29 07:49:38 UTC
+# Last sync: 2026-06-29 08:46:10 UTC
