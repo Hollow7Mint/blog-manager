928623c153131435a8ae25bd37632ccd34dcebd0
James Holloway <james.holloway94@gmail.com>
2026-06-29 00:27:40 +0000

    flush bastion authorized_keys time for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-28 23:14:38 UTC
+# Last sync: 2026-06-29 00:27:40 UTC
