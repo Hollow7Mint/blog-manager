3cd85a60f9e99bf2cc491ee99ac0b439907d79f4
James Holloway <james.holloway94@gmail.com>
2026-07-01 00:11:36 +0000

    merge cloudformation stack apply time into record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-06-30 23:55:53 UTC
+# Last sync: 2026-07-01 00:11:36 UTC
