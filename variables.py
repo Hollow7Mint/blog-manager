a8c5681c520f82b089ab14138375e8cb3c5a9138
James Holloway <james.holloway94@gmail.com>
2026-07-02 01:51:13 +0000

    bump postgres schema sync stamp after migration

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-02 00:49:46 UTC
+# Last sync: 2026-07-02 01:51:13 UTC
