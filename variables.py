9f7288e537ae34f6b9e220db23df2136c431baf1
James Holloway <james.holloway94@gmail.com>
2026-07-02 23:52:06 +0000

    merge mongodb shard sync time into metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-02 22:44:20 UTC
+# Last sync: 2026-07-02 23:52:06 UTC
