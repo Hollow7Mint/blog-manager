d1ceb149998c3c55148d2419dc0d01f3a9a6b5cb
James Holloway <james.holloway94@gmail.com>
2026-07-07 14:31:58 +0000

    catalog mongodb document count sync after audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-07 14:06:18 UTC
+# Last sync: 2026-07-07 14:31:58 UTC
