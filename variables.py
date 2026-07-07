69f1e818b4294d90dcd0dbcb4674bcdb69252092
James Holloway <james.holloway94@gmail.com>
2026-07-07 04:02:38 +0000

    flush mongodb replica lag marker for nightly check

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-07 03:05:51 UTC
+# Last sync: 2026-07-07 04:02:38 UTC
