88148f7e794370981cae602d621d36d8be6cb4d7
James Holloway <james.holloway94@gmail.com>
2026-07-07 16:46:16 +0000

    chart redis memory fragmentation sync in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-07 16:07:34 UTC
+# Last sync: 2026-07-07 16:46:16 UTC
