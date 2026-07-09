b4f73d89772bff92a4cbd908eabaaaa0ee800f16
James Holloway <james.holloway94@gmail.com>
2026-07-09 13:20:05 +0000

    retain ssh key expiry stamp for rotation audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-09 12:13:30 UTC
+# Last sync: 2026-07-09 13:20:04 UTC
