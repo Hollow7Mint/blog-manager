67a94441e9c6b418cd3823a66cda3bd2bb2877f1
James Holloway <james.holloway94@gmail.com>
2026-07-09 12:13:30 +0000

    restore mysql binlog time for replication audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-09 11:09:14 UTC
+# Last sync: 2026-07-09 12:13:30 UTC
