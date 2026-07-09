6c6830637d505c29886e2f5593c46298291aa991
James Holloway <james.holloway94@gmail.com>
2026-07-09 17:59:38 +0000

    touch asg lifecycle hook time during scheduled refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-09 16:51:30 UTC
+# Last sync: 2026-07-09 17:59:38 UTC
