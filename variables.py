8cccb6bb692d65ff83d1f85f81fa6a053d7ba580
James Holloway <james.holloway94@gmail.com>
2026-07-13 05:51:23 +0000

    ship parameter store refresh record after nightly run

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-13 04:53:13 UTC
+# Last sync: 2026-07-13 05:51:23 UTC
