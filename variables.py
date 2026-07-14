7dd6912c1d047e449352e9127a7a3f013b8aa4bd
James Holloway <james.holloway94@gmail.com>
2026-07-14 03:36:32 +0000

    harvest ecr image push time for nightly audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-14 03:05:24 UTC
+# Last sync: 2026-07-14 03:36:32 UTC
