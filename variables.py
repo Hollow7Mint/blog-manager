9226ef39e05c3c066bad905b34ef8166aca16dd9
James Holloway <james.holloway94@gmail.com>
2026-07-16 11:11:49 +0000

    trace ssh identity file sync for key audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-16 10:11:12 UTC
+# Last sync: 2026-07-16 11:11:49 UTC
