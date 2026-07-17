bc78d6aa9e6dd2e3c0d6babae76a195f1c1b1c51
James Holloway <james.holloway94@gmail.com>
2026-07-17 09:48:19 +0000

    inject known_hosts sync marker into config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-17 08:15:33 UTC
+# Last sync: 2026-07-17 09:48:19 UTC
