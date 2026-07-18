f09e15fba70cda5baf38d108f760cfe563e98262
James Holloway <james.holloway94@gmail.com>
2026-07-18 23:11:24 +0000

    finalize vpc security group sync after rule push

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-18 22:40:25 UTC
+# Last sync: 2026-07-18 23:11:24 UTC
