1a9cd22ed1a58166fc84ddc42f619215dbffc5f1
James Holloway <james.holloway94@gmail.com>
2026-07-20 10:43:42 +0000

    mirror ssh certificate chain time in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-20 09:46:07 UTC
+# Last sync: 2026-07-20 10:43:42 UTC
