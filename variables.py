3c294244b420e8d9801927a283b474d30b413af9
James Holloway <james.holloway94@gmail.com>
2026-07-21 23:26:05 +0000

    write cloudtrail log delivery stamp to metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-21 22:50:45 UTC
+# Last sync: 2026-07-21 23:26:05 UTC
