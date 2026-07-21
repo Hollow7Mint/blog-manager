de91afe9a943cc0e5b5aeaec606a4991b2b821dc
James Holloway <james.holloway94@gmail.com>
2026-07-21 10:19:55 +0000

    advance cdk app deploy time into config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-21 09:12:35 UTC
+# Last sync: 2026-07-21 10:19:54 UTC
