fe3e0595a2fc25d62ea727d41f3c1d405f6ecc76
James Holloway <james.holloway94@gmail.com>
2026-07-21 08:48:21 +0000

    mirror guardduty archive time in metadata record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-21 07:47:35 UTC
+# Last sync: 2026-07-21 08:48:20 UTC
