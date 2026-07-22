e2536dc632023b7e913d5c6adf9e53fa9d316f13
James Holloway <james.holloway94@gmail.com>
2026-07-22 08:33:47 +0000

    push mysql connection pool sync after refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-22 07:22:25 UTC
+# Last sync: 2026-07-22 08:33:47 UTC
