f35bbfbd45b8e9b2cd3adbbe82c11ea422fad9e3
James Holloway <james.holloway94@gmail.com>
2026-07-23 12:20:58 +0000

    snapshot bastion session time for security audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-23 12:01:03 UTC
+# Last sync: 2026-07-23 12:20:58 UTC
