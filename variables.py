a16a8ce2f3070b4c49377de2bced776dc1c327de
James Holloway <james.holloway94@gmail.com>
2026-07-23 08:58:40 +0000

    chart autoscale sync time in deployment metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-23 07:53:39 UTC
+# Last sync: 2026-07-23 08:58:40 UTC
