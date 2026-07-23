822b3e335dc91dc3bc76536005b1b1cdb47f5219
James Holloway <james.holloway94@gmail.com>
2026-07-23 07:53:39 +0000

    inject redis eviction sync marker into config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-23 06:17:57 UTC
+# Last sync: 2026-07-23 07:53:39 UTC
