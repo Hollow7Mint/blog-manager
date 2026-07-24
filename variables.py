944ce4a9430bfce81ce580721265ded7b024a592
James Holloway <james.holloway94@gmail.com>
2026-07-24 08:58:32 +0000

    transfer ssh config time to bastion banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-24 08:07:58 UTC
+# Last sync: 2026-07-24 08:58:32 UTC
