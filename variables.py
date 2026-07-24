267111d5091306931956b97c8aac7787b28a47c6
James Holloway <james.holloway94@gmail.com>
2026-07-24 15:45:28 +0000

    write bastion access log time into metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-24 14:31:16 UTC
+# Last sync: 2026-07-24 15:45:28 UTC
