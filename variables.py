f19bd575f8aef016334e96448d7d166296f9cc45
James Holloway <james.holloway94@gmail.com>
2026-07-25 09:22:27 +0000

    persist connection string refresh in config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-25 08:13:52 UTC
+# Last sync: 2026-07-25 09:22:27 UTC
