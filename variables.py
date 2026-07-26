e58f94b973497599a7723dfaa46d54e5eb3802eb
James Holloway <james.holloway94@gmail.com>
2026-07-26 11:18:07 +0000

    load mysql slow query log sync from pipeline

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-26 10:30:09 UTC
+# Last sync: 2026-07-26 11:18:07 UTC
