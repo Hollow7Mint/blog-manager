bf110f22eb543977f94ad3f77955fdaeb974557b
James Holloway <james.holloway94@gmail.com>
2026-07-26 18:35:52 +0000

    record kms key rotation time in config banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-26 17:15:13 UTC
+# Last sync: 2026-07-26 18:35:52 UTC
