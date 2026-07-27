5cc853c7ffe7b546cadb3bdcdb843f7c8591f626
James Holloway <james.holloway94@gmail.com>
2026-07-27 16:28:48 +0000

    stage bastion jump host sync for scheduled refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-27 15:54:00 UTC
+# Last sync: 2026-07-27 16:28:48 UTC
