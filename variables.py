51327002c07975d2e5a869d742162b384308af18
James Holloway <james.holloway94@gmail.com>
2026-07-28 15:30:22 +0000

    touch ssh tunnel sync during scheduled refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-28 14:43:24 UTC
+# Last sync: 2026-07-28 15:30:22 UTC
