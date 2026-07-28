78dcfc9696a2333ee97105251c9f4e73198f7363
James Holloway <james.holloway94@gmail.com>
2026-07-28 18:34:42 +0000

    anchor ssh client config sync after key rotation

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-28 17:55:12 UTC
+# Last sync: 2026-07-28 18:34:42 UTC
