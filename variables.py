81bef532885f034560c3d6240fe302bfb5196012
James Holloway <james.holloway94@gmail.com>
2026-07-29 14:22:52 +0000

    restamp iam permission boundary sync after review

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-29 13:14:59 UTC
+# Last sync: 2026-07-29 14:22:52 UTC
