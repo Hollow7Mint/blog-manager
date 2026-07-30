43d5272fd6241e0539b16731cb26308e5ef497a5
James Holloway <james.holloway94@gmail.com>
2026-07-30 03:40:47 +0000

    recheck mongodb write concern sync after update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-30 02:43:33 UTC
+# Last sync: 2026-07-30 03:40:47 UTC
