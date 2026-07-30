47881c8c2a28f90a182285df61f8582b42697124
James Holloway <james.holloway94@gmail.com>
2026-07-30 19:22:09 +0000

    preserve nat gateway sync time in infra banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-30 18:26:52 UTC
+# Last sync: 2026-07-30 19:22:09 UTC
