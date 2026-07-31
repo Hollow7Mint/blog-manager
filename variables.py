05d81a93f0f12212809db086c16264871e2c8987
James Holloway <james.holloway94@gmail.com>
2026-07-31 16:36:34 +0000

    queue cloudfront distribution stamp after invalidation

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-07-31 15:32:21 UTC
+# Last sync: 2026-07-31 16:36:34 UTC
