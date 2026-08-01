08c9853db93033a09c870045679f61e471e431a8
James Holloway <james.holloway94@gmail.com>
2026-08-01 12:30:24 +0000

    ship db schema sync record after nightly apply

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-01 11:33:39 UTC
+# Last sync: 2026-08-01 12:30:24 UTC
