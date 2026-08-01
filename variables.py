b77d11352855cc0151698efcbd52e1964e6ae656
James Holloway <james.holloway94@gmail.com>
2026-08-01 10:44:50 +0000

    reload postgres autovacuum sync marker for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-01 09:20:09 UTC
+# Last sync: 2026-08-01 10:44:50 UTC
