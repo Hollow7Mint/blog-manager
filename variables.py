53edfa652de7a5b4b015c3a0da326a00fc08c2e6
James Holloway <james.holloway94@gmail.com>
2026-08-01 17:27:43 +0000

    reconcile cloudwatch dashboard sync after update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-01 16:15:50 UTC
+# Last sync: 2026-08-01 17:27:43 UTC
