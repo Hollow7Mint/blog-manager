a63f9662b9280d6139a3a78e23b93e72b7e5cd54
James Holloway <james.holloway94@gmail.com>
2026-08-02 04:25:10 +0000

    stage rds connection pool sync for scheduled refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-02 03:13:16 UTC
+# Last sync: 2026-08-02 04:25:10 UTC
