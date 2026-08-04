435cdc3cfca6640143eca8da5a762f81774d8c56
James Holloway <james.holloway94@gmail.com>
2026-08-04 21:39:14 +0000

    audit vpc peering sync record after route update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-04 20:51:18 UTC
+# Last sync: 2026-08-04 21:39:14 UTC
