4f115c726067a4f6b6c17eee6ae98936b2835870
James Holloway <james.holloway94@gmail.com>
2026-08-04 03:41:25 +0000

    finalize mongodb collection sync after nightly apply

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-04 02:22:53 UTC
+# Last sync: 2026-08-04 03:41:25 UTC
