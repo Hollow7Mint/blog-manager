2941cebf115ea8b7d2993f882cddc7faf2df4069
James Holloway <james.holloway94@gmail.com>
2026-08-04 12:58:45 +0000

    post cdk bootstrap sync stamp to config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-04 11:35:37 UTC
+# Last sync: 2026-08-04 12:58:45 UTC
