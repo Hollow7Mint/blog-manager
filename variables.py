a5bde4b019d7269af732586e93b4f6e78e7a4293
James Holloway <james.holloway94@gmail.com>
2026-08-05 00:33:08 +0000

    adjust codebuild cache sync stamp after pipeline run

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-04 23:11:26 UTC
+# Last sync: 2026-08-05 00:33:08 UTC
