8dc406890583f8b61bcdb9496f69c59663717c80
James Holloway <james.holloway94@gmail.com>
2026-08-07 23:52:38 +0000

    recheck ecs service sync after deployment window

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-07 22:28:22 UTC
+# Last sync: 2026-08-07 23:52:38 UTC
