b3fffc9d448ee2511292652d3400b3ade533631d
James Holloway <james.holloway94@gmail.com>
2026-08-07 13:08:12 +0000

    bridge ssh ProxyJump config sync through banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-07 11:27:32 UTC
+# Last sync: 2026-08-07 13:08:12 UTC
