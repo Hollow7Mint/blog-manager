cfafb31f30c12ca675872cdc4b8a434a6d24e533
James Holloway <james.holloway94@gmail.com>
2026-08-07 17:07:49 +0000

    backfill s3 versioning sync marker in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-07 15:36:34 UTC
+# Last sync: 2026-08-07 17:07:49 UTC
