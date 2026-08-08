194c344949a19498016eca004658e726f765a7ad
James Holloway <james.holloway94@gmail.com>
2026-08-08 23:32:28 +0000

    touch redis sentinel sync during scheduled refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-08 22:11:38 UTC
+# Last sync: 2026-08-08 23:32:28 UTC
