776c090430e009f69d7f30ac3b7ea1d8483604a7
James Holloway <james.holloway94@gmail.com>
2026-08-10 22:00:00 +0000

    index redis sorted set sync during audit pass

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-10 20:25:23 UTC
+# Last sync: 2026-08-10 22:00:00 UTC
