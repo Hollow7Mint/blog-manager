d90ac3be00d955373baf84b2a8b86b8853a2f09d
James Holloway <james.holloway94@gmail.com>
2026-08-10 19:57:45 +0000

    resync parameter store path time for nightly audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-10 18:54:43 UTC
+# Last sync: 2026-08-10 19:57:45 UTC
