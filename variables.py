2d8193c853b8b3aa5b5dae26dae4eecb1ea73663
James Holloway <james.holloway94@gmail.com>
2026-08-10 16:21:56 +0000

    capture rds snapshot completion time in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-10 15:44:06 UTC
+# Last sync: 2026-08-10 16:21:56 UTC
