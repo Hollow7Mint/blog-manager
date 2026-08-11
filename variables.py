b4c5f72a576738f37d7e163260c34b7d8c11aa0e
James Holloway <james.holloway94@gmail.com>
2026-08-11 21:39:25 +0000

    rebuild nat gateway sync record after route change

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-11 20:53:16 UTC
+# Last sync: 2026-08-11 21:39:25 UTC
