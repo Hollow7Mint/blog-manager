1b697e32a9400f3d6deffa80585e99d8ca56d655
James Holloway <james.holloway94@gmail.com>
2026-08-12 05:40:04 +0000

    catalog rds parameter group sync after maintenance

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-12 04:43:14 UTC
+# Last sync: 2026-08-12 05:40:04 UTC
