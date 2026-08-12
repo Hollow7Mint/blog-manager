a85793171bf7edf1032be91d2906ba1751d29437
James Holloway <james.holloway94@gmail.com>
2026-08-12 22:01:29 +0000

    restore ssh public key time for rotation audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-12 20:13:03 UTC
+# Last sync: 2026-08-12 22:01:29 UTC
