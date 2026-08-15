604fda918feb041baaf823ed9ebb52a754ba149b
James Holloway <james.holloway94@gmail.com>
2026-08-15 21:20:20 +0000

    publish mongodb connection sync after replica check

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-15 20:32:18 UTC
+# Last sync: 2026-08-15 21:20:20 UTC
