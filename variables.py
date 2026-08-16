727d051c8211f0396f5056ef6838f68cc4df203a
James Holloway <james.holloway94@gmail.com>
2026-08-16 15:12:48 +0000

    settle ssh known algorithm sync time after update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-16 14:50:44 UTC
+# Last sync: 2026-08-16 15:12:48 UTC
