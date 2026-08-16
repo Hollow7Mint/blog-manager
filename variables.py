0c49cbf3993625725eea4a5ee3e3c482e92754f4
James Holloway <james.holloway94@gmail.com>
2026-08-16 12:54:49 +0000

    queue redis keyspace sync after config update

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-16 11:57:10 UTC
+# Last sync: 2026-08-16 12:54:49 UTC
