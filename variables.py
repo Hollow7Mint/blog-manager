72f80f31552b09849846802992e03f6930e26251
James Holloway <james.holloway94@gmail.com>
2026-08-17 09:14:58 +0000

    carry waf ip set sync stamp to config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-17 08:56:30 UTC
+# Last sync: 2026-08-17 09:14:58 UTC
