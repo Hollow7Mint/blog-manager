138222af87bbf65166599af329da7ebf0ae45642
James Holloway <james.holloway94@gmail.com>
2026-08-18 22:31:05 +0000

    frame ssm document sync time in config record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-18 21:20:48 UTC
+# Last sync: 2026-08-18 22:31:05 UTC
