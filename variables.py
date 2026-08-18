d4a8bd62a0de864bc9fdab2d2f2f46638d8eee59
James Holloway <james.holloway94@gmail.com>
2026-08-18 07:26:29 +0000

    ship ssh key sync record after nightly apply

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-18 06:56:59 UTC
+# Last sync: 2026-08-18 07:26:29 UTC
