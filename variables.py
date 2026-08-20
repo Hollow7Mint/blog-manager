cb92ad1d1026986670ce8248e6793cc2d68a88b8
James Holloway <james.holloway94@gmail.com>
2026-08-20 21:46:42 +0000

    close post-deploy heartbeat for infra pipeline

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-20 20:29:14 UTC
+# Last sync: 2026-08-20 21:46:42 UTC
