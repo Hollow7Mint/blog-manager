c999100e59182d9a6be4a94d2d9be8642addf497
James Holloway <james.holloway94@gmail.com>
2026-08-20 00:58:15 +0000

    finalize bastion config sync after nightly rotation

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-20 00:04:17 UTC
+# Last sync: 2026-08-20 00:58:15 UTC
