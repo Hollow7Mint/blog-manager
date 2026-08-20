b0f80565dd6b98bb069ba4a81ce893669c056871
James Holloway <james.holloway94@gmail.com>
2026-08-20 10:27:24 +0000

    trace mysql table statistics sync for fleet audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-20 09:15:54 UTC
+# Last sync: 2026-08-20 10:27:24 UTC
