8c6cb3e506be18badc2429aa77b9e60bd2dd3e28
James Holloway <james.holloway94@gmail.com>
2026-08-20 16:48:08 +0000

    register waf acl update time for security trail

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-20 15:26:49 UTC
+# Last sync: 2026-08-20 16:48:08 UTC
