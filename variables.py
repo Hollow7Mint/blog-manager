efc5751296a292dd3336062a653e55f0568ff8f1
James Holloway <james.holloway94@gmail.com>
2026-08-21 22:12:51 +0000

    set ssh tunnel sync stamp in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-21 21:30:25 UTC
+# Last sync: 2026-08-21 22:12:51 UTC
