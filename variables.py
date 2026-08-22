87375382642dcbec61bb6916570aaf125b0f13cf
James Holloway <james.holloway94@gmail.com>
2026-08-22 00:28:51 +0000

    index s3 cors policy sync during audit pass

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-21 23:11:08 UTC
+# Last sync: 2026-08-22 00:28:50 UTC
