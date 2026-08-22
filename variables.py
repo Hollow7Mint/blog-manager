197a05a58318c426b2fc6b75277fd347e6cd2f8a
James Holloway <james.holloway94@gmail.com>
2026-08-22 18:53:07 +0000

    inject ecs task definition marker into config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-22 17:32:06 UTC
+# Last sync: 2026-08-22 18:53:07 UTC
