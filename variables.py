fd8dfe89e932135ca99788a1865d367c6c47b8d3
James Holloway <james.holloway94@gmail.com>
2026-08-23 10:38:20 +0000

    bridge postgres foreign key sync through config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-23 09:59:51 UTC
+# Last sync: 2026-08-23 10:38:20 UTC
