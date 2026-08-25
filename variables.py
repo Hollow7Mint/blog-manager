28b27cadd2f90687929a20f51a274f5a4050d351
James Holloway <james.holloway94@gmail.com>
2026-08-25 08:55:28 +0000

    tag known_hosts sync checkpoint for key trail

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-25 07:25:04 UTC
+# Last sync: 2026-08-25 08:55:28 UTC
