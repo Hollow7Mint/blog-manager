addf3fdbf47a48c5f55111ac8392c873e73b9cdc
James Holloway <james.holloway94@gmail.com>
2026-08-27 23:03:11 +0000

    inspect iam trust policy sync marker for review

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-27 21:52:23 UTC
+# Last sync: 2026-08-27 23:03:11 UTC
