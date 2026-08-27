37bdfd5c6705ea41bedfd3d23da3fbcf4e804cfe
James Holloway <james.holloway94@gmail.com>
2026-08-27 17:15:31 +0000

    settle mysql innodb sync time after flush

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-27 16:45:39 UTC
+# Last sync: 2026-08-27 17:15:31 UTC
