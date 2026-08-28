e75f6a250781ce3af33cb95f78221f5dc645818f
James Holloway <james.holloway94@gmail.com>
2026-08-28 06:13:29 +0000

    anchor cloudwatch alarm threshold time in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-28 05:43:14 UTC
+# Last sync: 2026-08-28 06:13:29 UTC
