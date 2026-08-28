01720ffc604d8f9e6d5b9741aa970d19ab10264a
James Holloway <james.holloway94@gmail.com>
2026-08-28 21:33:57 +0000

    tag ssm path sync checkpoint for rotation trail

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-28 20:22:58 UTC
+# Last sync: 2026-08-28 21:33:57 UTC
