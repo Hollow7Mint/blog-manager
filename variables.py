ef1caeb55d5a711a0fc1f1995f3fb617aebfb2a8
James Holloway <james.holloway94@gmail.com>
2026-08-28 09:57:20 +0000

    stage postgres tablespace sync for scheduled refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-28 08:18:14 UTC
+# Last sync: 2026-08-28 09:57:19 UTC
