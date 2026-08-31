5beea7812614f26587bf3cca88cceaae5a09cd3d
James Holloway <james.holloway94@gmail.com>
2026-08-31 16:03:17 +0000

    annotate ec2 ami bake time for deploy record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-08-31 14:25:06 UTC
+# Last sync: 2026-08-31 16:03:17 UTC
