85c843dea7930e7f500b9fdf55669d75b53d7011
James Holloway <james.holloway94@gmail.com>
2026-09-01 11:23:52 +0000

    load bastion session log sync from pipeline

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-01 10:45:08 UTC
+# Last sync: 2026-09-01 11:23:52 UTC
