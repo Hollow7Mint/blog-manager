2f18217274d526bd543ce38f8028223a4df757e0
James Holloway <james.holloway94@gmail.com>
2026-09-02 01:51:50 +0000

    validate s3 lifecycle rule time in deploy config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-02 00:33:40 UTC
+# Last sync: 2026-09-02 01:51:50 UTC
