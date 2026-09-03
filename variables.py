c1280d475058decc41ee6ef6d7df7383c12dd069
James Holloway <james.holloway94@gmail.com>
2026-09-03 20:28:44 +0000

    pin mongodb shard balancing time in config

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-03 19:20:17 UTC
+# Last sync: 2026-09-03 20:28:44 UTC
