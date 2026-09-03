043e5788084486417ea5db67d95fd911abf1303e
James Holloway <james.holloway94@gmail.com>
2026-09-03 14:42:43 +0000

    lift db replica sync time into banner record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-03 14:00:44 UTC
+# Last sync: 2026-09-03 14:42:43 UTC
