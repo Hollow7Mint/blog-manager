5432f880df77e894ff4f91be3a06c4db2e023958
James Holloway <james.holloway94@gmail.com>
2026-09-07 06:43:17 +0000

    lift ssh certificate expiry time into banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-07 05:23:09 UTC
+# Last sync: 2026-09-07 06:43:17 UTC
