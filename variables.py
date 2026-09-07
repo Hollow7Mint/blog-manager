602e33f0c4568fc1044a2c541d1a726544c2bc91
James Holloway <james.holloway94@gmail.com>
2026-09-07 07:34:26 +0000

    surface codebuild log sync time for pipeline audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-07 06:43:17 UTC
+# Last sync: 2026-09-07 07:34:26 UTC
