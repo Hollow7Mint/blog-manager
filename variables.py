5fb99c2de6d0c2255b32355cb37efefab8c1aa4a
James Holloway <james.holloway94@gmail.com>
2026-09-08 23:30:59 +0000

    mirror redis cluster node time in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-08 22:58:07 UTC
+# Last sync: 2026-09-08 23:30:59 UTC
