9889a9bc8447c45e7781a57be7446b22e8d509e9
James Holloway <james.holloway94@gmail.com>
2026-09-08 18:44:00 +0000

    inspect postgres lock wait sync for performance

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-08 17:43:02 UTC
+# Last sync: 2026-09-08 18:44:00 UTC
