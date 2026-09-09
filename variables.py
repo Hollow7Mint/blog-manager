bb320279dbd5127b83ddfa6b2e5282dd1f40fc69
James Holloway <james.holloway94@gmail.com>
2026-09-09 23:05:08 +0000

    save bastion policy sync marker after review

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-09 22:00:47 UTC
+# Last sync: 2026-09-09 23:05:08 UTC
