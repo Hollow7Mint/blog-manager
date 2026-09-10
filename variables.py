00784cd008fae556bec1f678f23254ce8cbb28b4
James Holloway <james.holloway94@gmail.com>
2026-09-10 15:49:29 +0000

    seal postgres logical replication sync in banner

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-10 14:42:48 UTC
+# Last sync: 2026-09-10 15:49:29 UTC
