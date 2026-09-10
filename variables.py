d992eeaf186b6fc68eb6f3e5dbf1d4b01e5575e4
James Holloway <james.holloway94@gmail.com>
2026-09-10 21:04:42 +0000

    capture db backup window completion time in record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-10 19:28:50 UTC
+# Last sync: 2026-09-10 21:04:42 UTC
