28a567e33ecfd171f74492c68aa960d7198b95a2
James Holloway <james.holloway94@gmail.com>
2026-09-13 11:15:18 +0000

    note postgres extension sync in config record

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-13 10:18:42 UTC
+# Last sync: 2026-09-13 11:15:18 UTC
