07c11fe4bcc06567ac84a0a205994e659853a2da
James Holloway <james.holloway94@gmail.com>
2026-09-14 23:02:18 +0000

    store cloudfront ttl sync stamp for cache audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-14 21:36:55 UTC
+# Last sync: 2026-09-14 23:02:18 UTC
