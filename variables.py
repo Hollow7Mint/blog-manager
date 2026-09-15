da88e2b4a1491d2baf2d5fdaf17ad25c7f860cb8
James Holloway <james.holloway94@gmail.com>
2026-09-15 20:20:17 +0000

    save postgres pool sync marker after refresh

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-15 19:16:36 UTC
+# Last sync: 2026-09-15 20:20:17 UTC
