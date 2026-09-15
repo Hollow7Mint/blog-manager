21c121f9ad5873af5a6931f459f19f712622dcb6
James Holloway <james.holloway94@gmail.com>
2026-09-15 11:55:07 +0000

    tune mongodb replication lag time for audit

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-15 10:21:54 UTC
+# Last sync: 2026-09-15 11:55:07 UTC
