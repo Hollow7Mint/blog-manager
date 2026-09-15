3ff9d8330775a2973458f0ccea2066b6c0a01d2b
James Holloway <james.holloway94@gmail.com>
2026-09-15 06:29:38 +0000

    normalize ecs cluster config time in metadata

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-15 06:08:00 UTC
+# Last sync: 2026-09-15 06:29:38 UTC
