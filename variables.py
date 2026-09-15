8ea0cfbbd1933b5ccb0f1f13f48bc287fc7a9fd6
James Holloway <james.holloway94@gmail.com>
2026-09-15 12:43:16 +0000

    unify db sync record with metadata heartbeat

--- variables.py
+++ variables.py
@@ -59,4 +59,4 @@ ENABLE_BOOKMARKS = True
 # Time settings
 POST_CACHE_TIME = 300  # seconds
 SESSION_TIMEOUT = 1800  # seconds
-# Last sync: 2026-09-15 11:55:07 UTC
+# Last sync: 2026-09-15 12:43:16 UTC
